35dc0082947bf5683fa605a43ae86fb072eb5055
zanem1 <121981669+zanem1@users.noreply.github.com>
2023-09-03 11:18:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function openLink(url) {
+    var win = window.open();
+    var iframe = win.document.createElement('iframe');
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.border = "none";
+    iframe.src = url;
+    win.document.body.appendChild(iframe);
+}
+
+function search() {
+    var searchInput = document.getElementById("searchInput").value.toLowerCase();
+    var buttons = document.querySelectorAll(".button");
+    var noResultMessage = document.getElementById("noResultMessage");
+    var hasResults = false;
+
+    buttons.forEach(function(button) {
+        var buttonText = button.textContent.toLowerCase();
+        if (buttonText.includes(searchInput)) {
+            button.style.display = "block";
+            hasResults = true;
+        } else {
+            button.style.display = "none";
+        }
+    });
+
+    if (!hasResults) {
+        noResultMessage.style.display = "block";
+    } else {
+        noResultMessage.style.display = "none";
+    }
+}
 
